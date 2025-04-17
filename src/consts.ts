c440afb2df93d54c70f7052871d256b55ddf0df3
Rick Pfahl <pfahlr@gmail.com>
2025-04-16 20:43:29 -0400

    add configuration variables, store site setting in consts.ts, try to keep them to this one location, thanks

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,23 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const SITE_URL = 'https://example.com';
+export const SOCIAL_MEDIA = [{platform:"X", url:"https://x.com/pfahlr", cssicon:"twitter"},
+  {platform:"Github", url:"https://github.com/pfahlr", cssicon:"github"},
+  {platform:"LinkedIn", url:"https://linkedin.com/in/rickpfahl", cssicon:"linkedin"},
+  {platform:"RickPfahl.com", url:"http://rickpfahl.com", cssicon:"web"},
+  {platform:"Newliteracy.online", url:"http://newliteracy.online", cssicon:"web"},
+];
+export const SITE_TITLE = 'Astro SEO Starterkit SITE_TITLE';
+export const SITE_DESCRIPTION = 'This is the SITE_DESCRTIPTION';
+export const SITE_URL = 'https://astroseostarterkit.rickpfahl.com';
+export const SITE_KEYWORDS = ['keywordone, keyword2, keyword3'];
+export const SITE_SUBJECT = 'SITE_SUBJECT';
+export const COMPANY_NAME = 'rickpfahlllc';
+export const ADMIN_EMAIL = 'pfahlr@gmail.com';
+export const SITE_LANGUAGE = 'EN';
+export const ADSTXTLINE = "google.com, pub-0000000000000000, DIRECT, f08c47fec0942fa0";
+export const ROBOTSTXTCONTENT = `User-agent: *
+Allow: /
+
+Sitemap: `+SITE_URL+`/sitemap.xml
+`;
