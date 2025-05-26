32c5208ab72af2baa115fc66b5e9f342929d80f8
Rick Pfahl <pfahlr@gmail.com>
2025-05-25 20:04:59 -0400

    add ability to set image dimensions, set an optimization level to decrease filesize
    [
    
    '

--- src/utils/imageProcessor.ts
+++ src/utils/imageProcessor.ts
@@ -7,26 +7,49 @@ const isBuild = process.argv.includes('build');
 const IMAGE_DIR = isBuild ?  "dist/optimized-images" : "public/optimized-images";
 
 // Convert an image to WebP and return the new path
-export async function optimizeImage(originalPath: string): Promise<string> {
+export async function optimizeImage(originalPath: string, width: number = 0, height: number = 0): Promise<string> {
   if (!originalPath.startsWith("/")) return originalPath; // Ignore external images
 
-  const ext = path.extname(originalPath);
-  const baseName = path.basename(originalPath, ext);
-  const optimizedPath = `/optimized-images/${baseName}.webp`;
-  const fullInputPath = path.join(process.cwd(), "public", originalPath);
-  const fullOutputPath = path.join(process.cwd(), IMAGE_DIR, `${baseName}.webp`);
+  var ext = path.extname(originalPath);
+  var fullInputPath = path.join(process.cwd(), "public", originalPath);
+  var baseName = "";
+  var optimizedPath = "";
+  var fullOutputPath = "";
 
+
+  if ( width != 0 && height != 0) {
+    console.log(fullInputPath);
+    baseName = path.basename(originalPath, ext)+"-"+width+"x"+height;
+    console.log(baseName);
+    optimizedPath = `/optimized-images/${baseName}.webp`; 
+    console.log(baseName);
+    fullOutputPath = path.join(process.cwd(), IMAGE_DIR, `${baseName}.webp`);
+    console.log(fullOutputPath);
+  }
+  else {
+
+    baseName = path.basename(originalPath, ext);
+    console.log(baseName);
+    optimizedPath = `/optimized-images/${baseName}.webp`; 
+    fullOutputPath = path.join(process.cwd(), IMAGE_DIR, `${baseName}.webp`);
+  }
   // Ensure output directory exists
   fs.ensureDirSync(IMAGE_DIR);
+
 
   // Convert image if it doesn't already exist
   if (!fs.existsSync(fullOutputPath)) {
     try {
-      await sharp(fullInputPath).toFormat("webp").toFile(fullOutputPath);
+      if (width != 0 && height != 0) {
+        await sharp(fullInputPath).resize(width, height,  {fit:'cover'}).toFormat("webp",{quality: 70}).toFile(fullOutputPath);
+      }
+      else {
+        await sharp(fullInputPath).toFormat("webp",{quality: 80}).toFile(fullOutputPath);
+
+      }
     } catch (err) {
       console.error(`Error processing ${originalPath}:`, err);
     }
   }
-
   return optimizedPath;
 }
