a4a71f848f7ed237778960933ad8fca017c0dca9
Rick Pfahl <pfahlr@gmail.com>
2025-05-25 20:01:00 -0400

    add support for asset compression/minifiaction, scss/sass

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,44 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import remarkWebp from './src/plugins/remark-webp.js';
 import cloudflare from '@astrojs/cloudflare';
-
+import minify from 'astro-min';
 
 import { SITE_URL } from './src/consts';
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), 
+    /*minify({
+      /*
+       do_not_minify_doctype: false,
+       ensure_spec_compliant_unquoted_attribute_values: false,
+       keep_closing_tags: false,
+       keep_comments: false,
+       keep_html_and_head_opening_tags: false,
+       keep_input_type_text_attr: false,
+       keep_spaces_between_attributes: false,
+       keep_ssi_comments: false,
+       minify_css: true,
+       minify_js: true,
+       preserve_brace_template_syntax: false,
+       preserve_chevron_percent_template_syntax: false,
+       remove_bangs: false,
+       remove_processing_instructions: false,
+       
+    }),*/
+    (await import('astro-compress')).default({
+			CSS: true,
+			HTML: {
+				"html-minifier-terser": {
+					removeAttributeQuotes: false,
+				},
+			},
+			Image: true,
+			JavaScript: true,
+			SVG: true,      
+    }),
+  ],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
