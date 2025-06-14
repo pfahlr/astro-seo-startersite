780daea6b0f9b213e9ba885f33223c8f87326d64
Rick Pfahl <pfahlr@gmail.com>
2025-06-14 04:53:01 -0400

    move site information constants into a single dara object

--- src/pages/robots.txt.js
+++ src/pages/robots.txt.js
@@ -1,7 +1,7 @@
-import { ROBOTSTXTCONTENT } from '../consts';
+import { SITE_INFO } from '../consts';
 
 export async function GET() {
-  var content = new Response(ROBOTSTXTCONTENT);
+  var content = new Response(SITE_INFO.robotstxtcontent);
   content.headers.set('Content-Type', 'text/plain');
   return content;
 }
